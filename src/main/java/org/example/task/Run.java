9bfad1a767c0dd5f6fcc482a9d3c5660dca143c0
voylenkong@gmail.com <voylenkong@gmail.com>
2022-11-29 16:08:35 +0600

    secondTask

--- src/main/java/org/example/task/Run.java
+++ src/main/java/org/example/task/Run.java
@@ -3,6 +3,7 @@ package org.example.task;
 public class Run {
     public static void main(String[] args) {
 
-        System.out.println(Task.revertString("Hello World!"));
+        //System.out.println(Task.revertString344("Hello World!"));
+        System.out.println(Task.search704(new int[] {-10, -5, 0, 7, 10, 15, 16, 17, 18, 20, 25, 77, 88}, 17));
     }
 }
--- src/main/java/org/example/task/Task.java
+++ src/main/java/org/example/task/Task.java
@@ -3,7 +3,7 @@ package org.example.task;
 public class Task {
 
     //#344. Reverse String
-    public static String revertString(String inputStr) {
+    public static String revertString344(String inputStr) {
         char[] charsStr = inputStr.toCharArray();
         int lenOfCharStr = charsStr.length - 1;
         char buf;
@@ -15,5 +15,31 @@ public class Task {
            }
         }
         return new String(charsStr);
+    }
+
+    //#704. Binary Search
+    public static int search704(int[] nums, int target) {
+        int lp = 0;
+        int rp = nums.length - 1;
+        int cp = rp;
+
+        while (true) {
+            if (target == nums[lp]) {
+                return lp;
+            }
+            if (target == nums[rp]) {
+                return rp;
+            }
+
+            cp = (rp - lp) / 2;
+            if (target < nums[cp]) {
+                rp = cp;
+            } else {
+                lp = cp + 1;
+            }
+            if (cp == 0) {
+                return -1;
+            }
+        }
     }
 }
