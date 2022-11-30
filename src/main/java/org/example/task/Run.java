70144f170d5fafeb3ebee6e88fcdfc69bdfaa953
voylenkong@gmail.com <voylenkong@gmail.com>
2022-11-30 21:47:02 +0600

    412. Fizz Buzz

--- src/main/java/org/example/task/Run.java
+++ src/main/java/org/example/task/Run.java
@@ -9,5 +9,6 @@ public class Run {
         //System.out.println(Task.search704(new int[] {-10, -5, 0, 7, 10, 15, 16, 17, 18, 20, 25, 77, 88}, 17));
         //System.out.println(Task.fizzBuzz412(15));
 
+        
         }
 }
