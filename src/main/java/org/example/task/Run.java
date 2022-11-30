603e101e65a5deae947b4c2050e97252021f1478
voylenkong@gmail.com <voylenkong@gmail.com>
2022-11-30 21:55:42 +0600

    CLI

--- src/main/java/org/example/task/Run.java
+++ src/main/java/org/example/task/Run.java
@@ -8,7 +8,5 @@ public class Run {
         //System.out.println(Task.revertString344("Hello World!"));
         //System.out.println(Task.search704(new int[] {-10, -5, 0, 7, 10, 15, 16, 17, 18, 20, 25, 77, 88}, 17));
         //System.out.println(Task.fizzBuzz412(15));
-
-        
         }
 }
